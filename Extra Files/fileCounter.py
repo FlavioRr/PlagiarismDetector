aff41122bdb2d422a04d85d0394140dfaafb2214
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2024-06-01 23:06:00 -0600

    folder arrangement update

---
+++ Extra Files/fileCounter.py
@@ -0,0 +1,15 @@
+import os
+
+# Define the paths to the directories
+plagio_dir = r'C:\Users\Flavio Ruvalcaba\Documents\Escuela\Universidad\8Semestre\PlagiarismDetector\plagio'
+noplag_dir = r'C:\Users\Flavio Ruvalcaba\Documents\Escuela\Universidad\8Semestre\PlagiarismDetector\noplag'
+
+def count_files(directory):
+    return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])
+
+# Count files in each directory
+plagio_count = count_files(plagio_dir)
+noplag_count = count_files(noplag_dir)
+
+print(f'Cantidad de archivos en "plagio": {plagio_count}')
+print(f'Cantidad de archivos en "noplag": {noplag_count}')
