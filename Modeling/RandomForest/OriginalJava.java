73480212966427931d22e82a229005ee1036218b
Flavio Ruvalcaba Leija <71139354+FlavioRr@users.noreply.github.com>
2024-06-03 20:10:01 -0600

    test with another preprocess

--- Modeling/RandomForest/OriginalJava.java
+++ Modeling/RandomForest/OriginalJava.java
@@ -1,120 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class OriginalJava {
-    // Main method
     public static void main(String[] args) {
-        System.out.println("This is a large and complex Java file with many classes and methods.");
-        // Instantiate some classes
-        Class0 class0 = new Class0();
-        class0.method0();
-        
-        Class500 class500 = new Class500();
-        class500.method0();
-    }
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		int[] num = new int[10];
 
-    // Class 0
-    public static class Class0 {
-        public void method0() {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                list.add("Item " + i);
-            }
-            System.out.println("Class 0 Method 0: " + list);
-        }
-        public void method1() {
-            System.out.println("Class 0 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 0 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 0 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 0 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 0 Method 49");
-        }
-    }
+		for (int i = 0; i < 10; i++) {
+			// Read a number
+			System.out.print("Read a number: ");
 
-    // Class 1
-    public static class Class1 {
-        public void method0() {
-            System.out.println("Class 1 Method 0");
-        }
-        public void method1() {
-            System.out.println("Class 1 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 1 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 1 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 1 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 1 Method 49");
-        }
-    }
+			num[i] = input.nextInt();
+		}
 
-    // ... other classes ...
+		// Display the array
+		for (int i = 9; i >= 0; i--) {
+			System.out.println(num[i]);
+		}
+	}
 
-    // Class 499
-    public static class Class499 {
-        public void method0() {
-            System.out.println("Class 499 Method 0");
-        }
-        public void method1() {
-            System.out.println("Class 499 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 499 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 499 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 499 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 499 Method 49");
-        }
-    }
-
-    // Class 999
-    public static class Class999 {
-        public void method0() {
-            System.out.println("Class 999 Method 0");
-        }
-        public void method1() {
-            System.out.println("Class 999 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 999 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 999 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 999 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 999 Method 49");
-        }
-    }
-
-    // Utility class
-    public static class Utility {
-        public static void performAction(String message) {
-            System.out.println("Performing action with message: " + message);
-        }
-    }
 }
--- Modeling/RandomForest/PlagiarizedJava.java
+++ Modeling/RandomForest/PlagiarizedJava.java
@@ -1,147 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
 
 public class PlagiarizedJava {
-    // Main method
+    
     public static void main(String[] args) {
-        System.out.println("This is a large and complex Java file with many classes and methods.");
-        // Instantiate some classes
-        Class0 class0 = new Class0();
-        class0.method0();
-        
-        Class500 class500 = new Class500();
-        class500.method0();
-    }
-
-    // Class 0
-    public static class Class0 {
-        public void method0() {
-            List<String> list = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                list.add("Item " + i);
-            }
-            System.out.println("Class 0 Method 0: " + list);
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Read a number : ");
+            numbers[i] = sc.nextInt();
         }
-        public void method1() {
-            System.out.println("Class 0 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 0 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 0 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 0 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 0 Method 49");
-        }
-    }
-
-    // Class 1
-    public static class Class1 {
-        public void method0() {
-            System.out.println("Class 1 Method 0");
-        }
-        public void method1() {
-            System.out.println("Class 1 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 1 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 1 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 1 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 1 Method 49");
-        }
-    }
-
-    // ... other classes ...
-
-    // Class 499
-    public static class Class499 {
-        public void method0() {
-            System.out.println("Class 499 Method 0");
-        }
-        public void method1() {
-            System.out.println("Class 499 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 499 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 499 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 499 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 499 Method 49");
-        }
-    }
-
-    // Class 500
-    public static class Class500 {
-        public void method0() {
-            System.out.println("Class 500 Method 0 - Modified");
-        }
-        public void method1() {
-            System.out.println("Class 500 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 500 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 500 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 500 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 500 Method 49");
-        }
-    }
-
-    // Class 999
-    public static class Class999 {
-        public void method0() {
-            System.out.println("Class 999 Method 0");
-        }
-        public void method1() {
-            System.out.println("Class 999 Method 1");
-        }
-        public void method2() {
-            System.out.println("Class 999 Method 2");
-        }
-        public void method3() {
-            System.out.println("Class 999 Method 3");
-        }
-        public void method4() {
-            System.out.println("Class 999 Method 4");
-        }
-        // ... other methods ...
-        public void method49() {
-            System.out.println("Class 999 Method 49");
-        }
-    }
-
-    // Utility class
-    public static class Utility {
-        public static void performAction(String message) {
-            System.out.println("Performing action with message: " + message);
-        }
-
-        public static int addNumbers(int a, int b) {
-            return a + b;
+        for (int i = 9; i > -1; i--) {
+            System.out.println(numbers[i]);
         }
     }
 }
